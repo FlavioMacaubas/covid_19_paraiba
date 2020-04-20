9d589fc5075fba40a3b8b73b03ae027dd1eb2404
Flávio Macaúbas <49705807+FlavioMacaubas@users.noreply.github.com>
2020-04-20 16:02:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -340,7 +340,7 @@ app.layout = html.Div(
                         className='info-icon',
                     ),
                 ], className="container_title"),
-                html.Iframe(id='map', srcDoc=open("MAPA_COVID19.html", 'r').read(), width='100%', height='838'),
+                html.Iframe(id='map', srcDoc=open("MAPA_COVID19.html", 'r').read(), width='100%', height='600'),
             ], className='twelve columns pretty_container',
                 style={
                     'float' : 'left',
