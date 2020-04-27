ff5845bdec5341527f8418135ce908b8e61528d8
Flávio Macaúbas <49705807+FlavioMacaubas@users.noreply.github.com>
2020-04-27 02:55:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -350,7 +350,7 @@ app.layout = html.Div(
                                     ),
                                 ], className="container_title"),
                                 html.Iframe(id='map', srcDoc=open("MAPA_COVID19.html", 'r').read(), width='100%',
-                                            height=700),
+                                            height=650),
                             ], className='nine columns pretty_container',
                                 style={
                                     'float': 'left',
