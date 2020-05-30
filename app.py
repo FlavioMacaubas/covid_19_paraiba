be625f034afc539cca1525399f472d8d14ed7c19
Flávio Macaúbas <49705807+FlavioMacaubas@users.noreply.github.com>
2020-05-30 19:08:05 -0300

    Atualização 30/05

--- app.py
+++ app.py
@@ -256,7 +256,7 @@ app.layout = html.Div(
 
 
         dcc.Markdown(children=
-                     ''' > Atualização Covid-19 29/05 às 20:00h. Para melhor experiência acesse pelo computador.
+                     ''' > Atualização Covid-19 30/05 às 19:00h. Para melhor experiência acesse pelo computador.
         '''),
 
         dcc.Tabs([
