022aa74b962403f11288dbb65368d9996bd8db79
Flávio Macaúbas <49705807+FlavioMacaubas@users.noreply.github.com>
2020-08-08 20:05:35 -0300

    Atualização 08/08

--- app.py
+++ app.py
@@ -261,7 +261,7 @@ app.layout = html.Div(
         ], className='banner'),
 
         dcc.Markdown(children=
-                     ''' > Atualização Covid-19 07/08 às 19:30h. Para melhor experiência acesse pelo computador.
+                     ''' > Atualização Covid-19 08/08 às 20:30h. Para melhor experiência acesse pelo computador.
         '''),
 
         dcc.Tabs([
